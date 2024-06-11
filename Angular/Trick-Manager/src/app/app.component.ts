0c59f97949ad141966d399d5866e8782de788215
Nils Rindlisbacher <mex69@gmx.ch>
2024-06-11 21:15:15 +0200

    German Language Pack created
    i18n added to Trick-Manager
    German Language Pack created

--- Angular/Trick-Manager/src/app/app.component.ts
+++ Angular/Trick-Manager/src/app/app.component.ts
@@ -1,6 +1,7 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import {NavigationBarComponent} from "./navigation-bar/navigation-bar.component";
+import {Title} from "@angular/platform-browser";
 @Component({
   selector: 'app-root',
   standalone: true,
@@ -10,4 +11,8 @@ import {NavigationBarComponent} from "./navigation-bar/navigation-bar.component"
 })
 export class AppComponent {
   title = 'Trick-Manager';
+
+  constructor(private titleService: Title) {
+    this.titleService.setTitle($localize`${this.title}`)
+  }
 }
