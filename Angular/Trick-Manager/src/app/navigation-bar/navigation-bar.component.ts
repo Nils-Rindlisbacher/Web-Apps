a913ae732fc3e3ab3b8a553344b84f36f01adc20
Nils Rindlisbacher <mex69@gmx.ch>
2024-06-13 00:07:12 +0200

    Dark Mode added
    Switch to dark mode is now possible, but resets on page change

--- Angular/Trick-Manager/src/app/navigation-bar/navigation-bar.component.ts
+++ Angular/Trick-Manager/src/app/navigation-bar/navigation-bar.component.ts
@@ -1,15 +1,26 @@
 import { Component } from '@angular/core';
 import {NgOptimizedImage} from "@angular/common";
+import { ThemeService } from '../theme.service';
+import {MatIconModule} from '@angular/material/icon';
 
 @Component({
   selector: 'app-navigation-bar',
   standalone: true,
   imports: [
-    NgOptimizedImage
+    NgOptimizedImage, MatIconModule
   ],
   templateUrl: './navigation-bar.component.html',
   styleUrl: './navigation-bar.component.scss'
 })
 export class NavigationBarComponent {
+  isDarkMode: boolean;
 
+  constructor(private themeService: ThemeService) {
+    this.isDarkMode = this.themeService.isDarkMode();
+  }
+
+  toggleTheme() {
+    this.isDarkMode = !this.isDarkMode;
+    this.themeService.setDarkMode(this.isDarkMode);
+  }
 }
