fadabd1c9ce2f518a5e718724721cb20b0b65d98
Nils Rindlisbacher <mex69@gmx.ch>
2024-06-27 09:48:52 +0200

    Update TrickController.java

--- Angular/Trick-Manager/Trick_API/src/main/java/com/trick_manager/Trick_API/controller/TrickController.java
+++ Angular/Trick-Manager/Trick_API/src/main/java/com/trick_manager/Trick_API/controller/TrickController.java
@@ -21,23 +21,14 @@ public class TrickController {
     this.trickService = trickService;
   }
 
-  /**
-   * Create a new trick.
-   *
-   * @param trick the trick to create
-   * @return the ResponseEntity with status 200 (OK) and with body of the new trick
-   */
+
   @PostMapping("/trick")
   public ResponseEntity<Trick> saveTrick(@RequestBody Trick trick) {
     Trick newTrick = trickService.saveTrick(trick);
     return ResponseEntity.ok(newTrick);
   }
 
-  /**
-   * Get all tricks.
-   *
-   * @return the ResponseEntity with status 200 (OK) and with body of the list of tricks
-   */
+
   @GetMapping("/tricks")
   public List<Trick> getAllTricks() {
     return trickService.getAllTricks();
@@ -48,37 +39,20 @@ public class TrickController {
     return trickService.getAllTypes();
   }
 
-  /**
-   * Get a trick by ID.
-   *
-   * @param id the ID of the trick to get
-   * @return the ResponseEntity with status 200 (OK) and with body of the trick, or with status 404 (Not Found) if the trick does not exist
-   */
   @GetMapping("/trick/{id}")
   public ResponseEntity<Trick> getTrickById(@PathVariable Long id) {
     Optional<Trick> trick = trickService.getTrickById(id);
     return trick.map(ResponseEntity::ok).orElseGet(() -> ResponseEntity.notFound().build());
   }
 
-  /**
-   * Update a trick by ID.
-   *
-   * @param id the ID of the trick to update
-   * @param trick the updated trick
-   * @return the ResponseEntity with status 200 (OK) and with body of the updated trick, or with status 404 (Not Found) if the trick does not exist
-   */
+
   @PutMapping("/trick/{id}")
   public ResponseEntity<Trick> updateTrick(@PathVariable Long id, @RequestBody Trick trick) {
     Trick updatedTrick = trickService.updateTrick(id, trick);
     return ResponseEntity.ok(updatedTrick);
   }
 
-  /**
-   * Delete a trick by ID.
-   *
-   * @param id the ID of the trick to delete
-   * @return the ResponseEntity with status 200 (OK) and with body of the message "Trick deleted successfully"
-   */
+
   @DeleteMapping("/trick/{id}")
   public ResponseEntity<String> deleteTrick(@PathVariable Long id) {
     trickService.deleteTrick(id);
