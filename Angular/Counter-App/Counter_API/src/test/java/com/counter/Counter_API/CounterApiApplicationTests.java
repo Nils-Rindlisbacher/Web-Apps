de53b3e01d57e981a49bd720705a3dab5150e13c
Nils Rindlisbacher <mex69@gmx.ch>
2024-07-04 01:09:50 +0200

    Created Counter REST API

---
+++ Angular/Counter-App/Counter_API/src/test/java/com/counter/Counter_API/CounterApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.counter.Counter_API;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CounterApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
